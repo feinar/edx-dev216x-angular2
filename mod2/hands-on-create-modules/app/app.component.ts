ce27c8b18eb0db921a61ca42f1bbe4e08c499d67
feinar <feinar@gmail.com>
2016-08-23 12:39:56 +0300

    Module2. Hands-on: Created module

--- mod2/hands-on-create-modules/app/app.component.ts
+++ mod2/hands-on-create-modules/app/app.component.ts
@@ -1,8 +1,15 @@
 import {Component} from '@angular/core';
+import {TextGenerator} from './demo.module';
 
 @Component({
   selector: 'demo-app',
   templateUrl: 'app/app.component.html',
   directives:  []
 })
-export class AppComponent { }
+export class AppComponent { 
+	public message : string;
+	constructor() {
+		let generator = new TextGenerator();
+		this.message = generator.GetText();
+	}
+}
